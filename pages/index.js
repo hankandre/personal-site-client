a3fdf5dd1eb0567cd4b7a9a0b02f59590f2e8bb9
Hank Andre <hankandre@gmail.com>
2017-07-14 23:25:22 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+const Index = () =>
+  <main>
+    <style jsx global>{`
+      body {
+        font-family: Futura, 'Helvetica Neue', Helvetica, sans-serif;
+      }
+    `}</style>
+    <h3>Hello</h3>
+  </main>;
+
+export default Index;
